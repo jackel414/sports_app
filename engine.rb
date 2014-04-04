014521075b6e55803d2d912ed80709fad21f151a
Zachary Mays <zackmays@gmail.com>
2014-04-04 14:23:43 -0400

    Add config file, enhance regexp

--- engine.rb
+++ engine.rb
@@ -5,9 +5,10 @@ mlb_url = 'http://sports.espn.go.com/mlb/bottomline/scores'
 mlb_scores = open(mlb_url) { |io| data = io.read}
 mlb_scores.gsub!(/%20/, ' ')
 redsox_status = nil
-puts "Enter your team's city:"
-team = gets.chomp
+# puts "Enter your team's city:"
+# team = gets.chomp
 
+=begin
 if mlb_scores =~ /#{team}/
   if mlb_scores =~ /#{team} at/ or mlb_scores =~ /at #{team}/
     redsox_status = 'not over'
@@ -28,11 +29,40 @@ else
   redsox_status = 'not today'
   message = "#{team} does not play today."
 end
+=end
 
+=begin
+if mlb_scores =~ /#{team}/
+  if mlb_scores =~ /(#{team})(?<==)/
+    puts 'True'
+  else
+    puts 'Not there'
+  end
+end
+=end
+
+# (Boston)(?=\s\d) is for a space and then number
+
+=begin
 if redsox_status != 'not today'
   puts "The game is #{redsox_status}. #{message}"
 else
   puts message
 end
+=end
 
-#puts mlb_scores
+red_sox_game = nil
+mlb_scores.scan(mlb_scores).each do |match| 
+    scores = match.scan(/((?<==)([\^][[:upper:]]|[[:upper:]][[:lower:]])[^\(&]{1,})/)
+    scores.each do |score|
+      if score[0] =~ /Boston/
+        red_sox_game = score[0]
+      end
+    end
+end
+
+puts red_sox_game
+game_position = mlb_scores =~ /#{red_sox_game}/
+game_status = mlb_scores[game_position..game_position+50].match(/(?<=\()[^\)]{1,}/)
+puts game_status
+
